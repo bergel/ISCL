91dbe93714ffc63f50085f89b8659b5f7ac3d512
Alexandre Bergel <alexandre.bergel@me.com>
2020-05-26 15:16:33 -0400

    improved the add_news.sh script

--- _news/add_news.sh
+++ _news/add_news.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-if [ "$#" -ne 1 ]; then
-	echo "This is a script to generate a news."
-    echo "You need to enter a title as argument of the command. E.g., ./add_news.sh \"NewPCMember\""
-    echo "The argument is used as a filename."
-    exit
-fi
+# if [ "$#" -ne 1 ]; then
+# 	echo "This is a script to generate a news."
+#     echo "You need to enter a title as argument of the command. E.g., ./add_news.sh \"NewPCMember\""
+#     echo "The argument is used as a filename."
+#     exit
+# fi
 
-FILENAME=`date "+%Y-%m-%d"`-"$1".md
+FILENAME=`date "+%Y-%m-%d"`.md
 touch "$FILENAME"
 echo "--- 
 layout: post 
